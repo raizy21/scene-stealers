ab5fbf3d70ac4126991d50f0d9349a86d1e5bf2f
Johanna <johanna.k.1996@googlemail.com>
2025-01-20 19:15:36 +0100

    basic HTML and Tailwind Setup, added Logo and different fonts, and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,35 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        primaryColor: "#123456",
-        secondaryColor: "#123456",
-        accentColor: "#123456",
-        backgroundColor: "#123456",
-        textColor: "#123456",
-      },
-      fontFamily: {
-        sans: ["Ubuntu", "system-ui"],
-      },
-      screens: {
-        sm: "640px",
-        // => @media (min-width: 640px) { ... }
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./*.{html,js}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primaryColor: "#4845d2",
+//         secondaryColor: "#a5b4fc",
+//         accentColor: "#4845d2",
+//         backgroundColor: "#a5b4fc",
+//         textColor: "#0e0023",
+//       },
+//       fontFamily: {
+//         sans: ["Bungee", "system-ui"],
+//       },
+//       screens: {
+//         sm: "640px",
+//         // => @media (min-width: 640px) { ... }
 
-        tablet: "768px",
-        // => @media (min-width: 	768px) { ... }
+//         tablet: "768px",
+//         // => @media (min-width: 	768px) { ... }
 
-        miniLaptop: "900px",
-        // => @media (min-width: 900px) { ... }
+//         miniLaptop: "900px",
+//         // => @media (min-width: 900px) { ... }
 
-        laptop: "1024px",
-        // => @media (min-width: 1024px) { ... }
+//         laptop: "1024px",
+//         // => @media (min-width: 1024px) { ... }
 
-        desktop: "1280px",
-        // => @media (min-width: 1280px) { ... }
-      },
-    },
-  },
-  plugins: [],
-};
+//         desktop: "1280px",
+//         // => @media (min-width: 1280px) { ... }
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
